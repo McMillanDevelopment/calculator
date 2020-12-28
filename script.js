f1743520b53a08d698577f6470a4f410e71cdc69
Michael McMillan <mcmillanmikea@gmail.com>
2020-12-28 15:42:07 -0800

    simple +-*/ operations now working, only works one operation at a time ie. 1 + 1 = 2, not 1 + 1 + 1 = 3

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // DOM Selectors
-const buttons = document.querySelectorAll("#container div")
-const display = document.querySelector("#display p")
-const clear = document.querySelector("#clear")
+const buttons = document.querySelectorAll("#container div");
+const display = document.querySelector("#display p");
+const clear = document.querySelector("#clear");
+const equals = document.querySelector("#equals");
 
+const divideButton = document.querySelector("#divide")
+const multiplyButton = document.querySelector("#multiply")
+const plusButton = document.querySelector("#plus")
+const subtractButton = document.querySelector("#subtract")
 
 // Event Listners
 display.addEventListener('click', function(e){
@@ -21,6 +26,8 @@ for(i = 3; i < buttons.length; i++) {
 
 clear.addEventListener('click', function(){
     display.textContent = 0;
+    firstValue = 0;
+    secondValue = 0;
 });
 
 // add function
@@ -43,6 +50,62 @@ const divide = function(a, b) {
     return a / b;
 };
 
+// variables
+firstValue = 0;
+secondValue = 0;
+
+// Function call event listeners
+plusButton.addEventListener('click', function(e){
+    let x = display.textContent.slice(0, display.textContent.length - 1)
+    firstValue = Number(x);
+    // console.log(firstValue)
+});
+
+subtractButton.addEventListener('click', function(e){
+    let x = display.textContent.slice(0, display.textContent.length - 1)
+    firstValue = Number(x);
+    // console.log(firstValue)
+});
+
+multiplyButton.addEventListener('click', function(e){
+    let x = display.textContent.slice(0, display.textContent.length - 1)
+    firstValue = Number(x);
+    // console.log(firstValue)
+});
+
+divideButton.addEventListener('click', function(e){
+    let x = display.textContent.slice(0, display.textContent.length - 1)
+    firstValue = Number(x);
+    // console.log(firstValue)
+});
+
+
+equals.addEventListener('click', function(){
+    let stringer = String(firstValue);
+    let a = stringer.length + 1
+    let b = display.textContent.length;
+    secondValue = Number(display.textContent.slice(a, b))
+    // let x = display.textContent.slice(2, 3);
+    // console.log(`firstValue: ${firstValue} secondValue: ${ secondValue}`) // temporary
+
+
+    // addition, subtract function
+    let string = display.textContent;
+    console.log(`string: ${string}`);
+    if(string.includes('+')) {
+        string = add(firstValue, secondValue);
+        display.textContent = string;
+    } else if (string.includes('-')) {
+        string = subtract(firstValue, secondValue);
+        display.textContent = string;
+    } else if (string.includes('*')) {
+        string = multiply(firstValue, secondValue);
+        display.textContent = string;
+    } else if (string.includes('/')) {
+        string = divide(firstValue, secondValue);
+        display.textContent = string;
+    }
+});
 
 
 // operate function
