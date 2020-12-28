ea9d07e6ab34a9b46499c1ddc113907ba304bb92
Michael McMillan <mcmillanmikea@gmail.com>
2020-12-28 13:23:13 -0800

    Added basic display functionality and a clear button to reset display to default

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// DOM Selectors
+const buttons = document.querySelectorAll("#container div")
+const display = document.querySelector("#display p")
+const clear = document.querySelector("#clear")
+
+
+// Event Listners
+display.addEventListener('click', function(e){
+    console.log(e.target.textContent);
+});
+
+for(i = 3; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function(e){
+    // Resets display text before adding clicked buttons
+    if(display.textContent === String(0)) {
+        display.textContent = "";
+    };
+    display.textContent = display.textContent + e.target.textContent;
+  });
+};
+
+clear.addEventListener('click', function(){
+    display.textContent = 0;
+});
+
 // add function
 const add = function (a, b) {
     return a + b;
@@ -18,17 +43,19 @@ const divide = function(a, b) {
     return a / b;
 };
 
+
+
 // operate function
 // takes 2 numbers and calls one of the above functions.
-const operate = function(a, b) {
-    if(mouseclick unicode === +) {
-        return add(a, b);
-    } else if (mouseclick unicode === -) {
-        return subtract(a, b);
-    } else if (mouseclick unicode === *) {
-        return multiply(a, b);
-    } else if (mouseclick unicode === /) {
-        return divide(a, b);
-    };
-};
+// const operate = function(a, b) {
+//     if(mouseclick unicode === +) {
+//         return add(a, b);
+//     } else if (mouseclick unicode === -) {
+//         return subtract(a, b);
+//     } else if (mouseclick unicode === *) {
+//         return multiply(a, b);
+//     } else if (mouseclick unicode === /) {
+//         return divide(a, b);
+//     };
+// };
 // I think that will work?
